a3003facd332f59eeedbad34e1f8868e4e062169
adambunce <adam.bunce11@gmail.com>
2024-02-14 10:09:44 -0500

    fix stat cycle on start

--- stats/stats.go
+++ stats/stats.go
@@ -17,7 +17,11 @@ func StatResetCycle() {
 	for {
 		now := time.Now().UTC()
 
-		globals.SendWebhook(fmt.Sprintf(":arrows_clockwise: - METAR/GFA pages loaded %d time(s)", serveCount.Load()))
+		if serveCount.Load() == 0 {
+			globals.SendWebhook(fmt.Sprintf("Deployed v%s", globals.Version))
+		} else {
+			globals.SendWebhook(fmt.Sprintf(":arrows_clockwise: - METAR/GFA pages loaded %d time(s)", serveCount.Load()))
+		}
 		serveCount.Store(0)
 
 		nextMidnight := time.Date(now.Year(), now.Month(), now.Day()+1, 0, 0, 0, 0, time.UTC)
