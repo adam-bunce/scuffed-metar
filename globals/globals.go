ad2ded699a1ce8aa5deec7d1fed816d7d828a8c5
adambunce <adam.bunce11@gmail.com>
2024-02-27 22:33:11 -0500

    fix env

--- globals/globals.go
+++ globals/globals.go
@@ -11,7 +11,7 @@ import (
 
 var ServerPort = 80
 var webhook = os.Getenv("WEBHOOK_URL")
-var Env = "local"
+var Env = "prod"
 var Version = "1.7.5" // for cache busting
 
 var VersionHistory = []struct {
